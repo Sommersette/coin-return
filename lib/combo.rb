daa64384037f5921b00f4e0f74712e06d7f56405
Dominic Brown and Trevor Fast <dominic+trevor@>
2017-04-20 11:03:47 -0700

    small refactor

--- lib/combo.rb
+++ lib/combo.rb
@@ -1,51 +1,54 @@
-
 class Fixnum
   define_method(:coin_return) do
     final_arr = []
     working_num = self
-
     quarter_back = 0
     dimebag_back = 0
     nickel_back = 0
     pennywise_back = 0
 
-    coin_hash = {
-      25 => "quarters",
-      10 => "dimes",
-      5 => "nickels",
-      1 => "pennies"
-    }
-
     if ((working_num / 25) >= 1)
       quarter_back = (working_num / 25).floor()
       final_arr.push(quarter_back.to_s)
-      final_arr.push(coin_hash[25])
+      final_arr.push("quarters")
       working_num = (working_num - (quarter_back * 25))
     end
-
     if ((working_num / 10) >= 1)
       dimebag_back = (working_num / 10).floor()
       final_arr.push(dimebag_back.to_s)
-      final_arr.push(coin_hash[10])
+      final_arr.push("dimes")
       working_num = (working_num - (dimebag_back * 10))
     end
-
     if ((working_num/ 5) >= 1)
       nickel_back = (working_num / 5).floor()
       final_arr.push(nickel_back.to_s)
-      final_arr.push(coin_hash[5])
+      final_arr.push("nickels")
       working_num = (working_num - (nickel_back * 5))
     end
-
     if ((working_num % 5) > 0)
       pennywise_back = (working_num % 5)
       final_arr.push(pennywise_back.to_s)
-      final_arr.push(coin_hash[1])
+      final_arr.push("pennies")
     end
 
     final_arr.join(" ")
   end
 end
+
+    # coin_hash = {
+    #   25 => "quarters",
+    #   10 => "dimes",
+    #   5 => "nickels",
+    #   1 => "pennies"
+    # }
+
+    # while (num != 0) do
+    #
+    #   working_num -= stuff
+    # end
+
+
+
 
 
 # counter = 1
